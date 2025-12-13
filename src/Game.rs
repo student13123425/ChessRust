06b0857c4d74d23268d66e13816ffda4c6f63ae5
NicolaeMihaiAlexandru <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-12-14 00:21:00 +0200

    initialized Game class

--- src/Game.rs
+++ src/Game.rs
@@ -1,6 +1,16 @@
+use crate::Aox::Vec2D;
 use crate::Board::Board;
 
 struct Game {
-    board: Board
+    board: Board,
+    selected_pice:Vec2D
+}
 
+impl Game {
+    pub fn new() -> Self {
+        Self {
+            board: Board::new(),
+            selected_pice:Vec2D::new(-1,-1)
+        }
+    }
 }

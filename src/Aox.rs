23b5b9d02b006c550f97a14bf96ff0ecab826f06
AndreiSimoiu <245866203+student13123425@users.noreply.github.com>
2025-12-13 12:21:19 +0200

    Helper Objects

---
+++ src/Aox.rs
@@ -0,0 +1,43 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Vec2D {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Vec2D {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Rect2D {
+    pub x: i32,
+    pub y: i32,
+    pub width: i32,
+    pub height: i32,
+}
+
+impl Rect2D {
+    pub fn new(x: i32, y: i32, width: i32, height: i32) -> Self {
+        Self {
+            x,
+            y,
+            width,
+            height,
+        }
+    }
+    
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Line2D {
+    pub start: Vec2D,
+    pub end: Vec2D,
+}
+
+impl Line2D {
+    pub fn new(start: Vec2D, end: Vec2D) -> Self {
+        Self { start, end }
+    }
+}

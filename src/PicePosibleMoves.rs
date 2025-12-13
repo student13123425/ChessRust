eebe2d1fe9946d23751a5388c6f0b2e179931940
NicolaeMihaiAlexandru <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-12-14 00:07:19 +0200

    RookMove generation implemented

--- src/PicePosibleMoves.rs
+++ src/PicePosibleMoves.rs
@@ -15,42 +15,142 @@ fn get_id_side(id: i32) -> i32 {
 pub fn pawn_move(pice: Pice, board: &Board) -> Vec<Vec2D> {
     let mut out: Vec<Vec2D> = vec![];
     let board_state = board.get_board_state();
+    let mut o = 1;
+    if pice.side {
+        o = -1;
+    }
+    let front_x = pice.pos.x + o;
+    let mut side = 2;
+    if !pice.side {
+        side = 1;
+    }
 
-    let direction = if pice.side { -1 } else { 1 };
-    let start_x = if pice.side { 6 } else { 1 };
+    let mut s1 = 0;
+    let mut s2 = 0;
 
-    let current_x = pice.pos.x;
-    let current_y = pice.pos.y;
-
-    let next_x = current_x + direction;
-
-    if next_x >= 0 && next_x < 8 {
-        if board_state[next_x as usize][current_y as usize] == -1 {
-            out.push(Vec2D::new(next_x, current_y));
-
-            if current_x == start_x {
-                let double_x = current_x + (direction * 2);
-                if double_x >= 0 && double_x < 8 {
-                    if board_state[double_x as usize][current_y as usize] == -1 {
-                        out.push(Vec2D::new(double_x, current_y));
-                    }
-                }
-            }
+    if front_x >= 0 && front_x < 8 {
+        if pice.pos.y - 1 >= 0 {
+            let v1 = board_state[front_x as usize][(pice.pos.y - 1) as usize];
+            s1 = get_id_side(v1);
         }
-
-        let capture_ys = [current_y - 1, current_y + 1];
-        for &cap_y in &capture_ys {
-            if cap_y >= 0 && cap_y < 8 {
-                let target_id = board_state[next_x as usize][cap_y as usize];
-                let target_side = get_id_side(target_id);
-                let my_side = if pice.side { 1 } else { 2 };
-
-                if target_id != -1 && target_side != my_side {
-                    out.push(Vec2D::new(next_x, cap_y));
-                }
-            }
+        if pice.pos.y + 1 < 8 {
+            let v2 = board_state[front_x as usize][(pice.pos.y + 1) as usize];
+            s2 = get_id_side(v2);
         }
     }
 
-  return out
+    if s1 == side {
+        out.push(Vec2D::new(front_x, pice.pos.y - 1))
+    }
+    if s2 == side {
+        out.push(Vec2D::new(front_x, pice.pos.y + 1))
+    }
+
+    for i in 1..=2 {
+        let x_value = pice.pos.x + i * o;
+
+        if x_value < 0 || x_value >= 8 {
+            break;
+        }
+
+        if i == 2 {
+            if pice.side && pice.pos.x != 6 {
+                break;
+            }
+            if !pice.side && pice.pos.x != 1 {
+                break;
+            }
+        }
+
+        let value = get_id_side(board_state[x_value as usize][pice.pos.y as usize]);
+        if value != 0 {
+            break;
+        } else {
+            out.push(Vec2D::new(x_value, pice.pos.y))
+        }
+    }
+    out
 }
+
+
+
+pub fn rook_moves(pice: Pice, board: &Board) -> Vec<Vec2D> {
+    let mut out: Vec<Vec2D> = vec![];
+    let pos = pice.pos;
+    let board_state = board.get_board_state();
+    let mut side = 2;
+    if !pice.side {
+        side = 1;
+    }
+    let mut arr = vec![false, false, false, false];
+
+    for v in 1..8 {
+        let v_i32 = v as i32;
+
+        if !arr[0] {
+            if pos.x - v_i32 >= 0 {
+                let v1 = get_id_side(board_state[(pos.x - v_i32) as usize][pos.y as usize]);
+                if v1 != side {
+                    if v1 != 0 {
+                        arr[0] = true;
+                    }
+                    out.push(Vec2D::new(pos.x - v_i32, pos.y));
+                } else {
+                    arr[0] = true;
+                }
+            } else {
+                arr[0] = true;
+            }
+        }
+
+        if !arr[1] {
+            if pos.x + v_i32 < 8 {
+                let v2 = get_id_side(board_state[(pos.x + v_i32) as usize][pos.y as usize]);
+                if v2 != side {
+                    if v2 != 0 {
+                        arr[1] = true;
+                    }
+                    out.push(Vec2D::new(pos.x + v_i32, pos.y));
+                } else {
+                    arr[1] = true;
+                }
+            } else {
+                arr[1] = true;
+            }
+        }
+
+        if !arr[2] {
+            if pos.y - v_i32 >= 0 {
+                let v3 = get_id_side(board_state[pos.x as usize][(pos.y - v_i32) as usize]);
+                if v3 != side {
+                    if v3 != 0 {
+                        arr[2] = true;
+                    }
+                    out.push(Vec2D::new(pos.x, pos.y - v_i32));
+                } else {
+                    arr[2] = true;
+                }
+            } else {
+                arr[2] = true;
+            }
+        }
+
+        if !arr[3] {
+            if pos.y + v_i32 < 8 {
+                let v4 = get_id_side(board_state[pos.x as usize][(pos.y + v_i32) as usize]);
+                if v4 != side {
+                    if v4 != 0 {
+                        arr[3] = true;
+                    }
+                    out.push(Vec2D::new(pos.x, pos.y + v_i32));
+                } else {
+                    arr[3] = true;
+                }
+            } else {
+                arr[3] = true;
+            }
+        }
+    }
+    return out
+}
+
